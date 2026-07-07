32b9ef2c1592f1fc2b9b0c8fce1d1ed51b64893f
satokae <208755468+satoKae@users.noreply.github.com>
2026-07-07 13:51:11 +0900

    judgement

--- test_PGM2/Main.java
+++ test_PGM2/Main.java
@@ -1,16 +1,37 @@
+import java.util.function.BiFunction;
+
 public class Main {
 
+  static void test(BiFunction<Float, Integer, Long> callback, String name) {
+    boolean isPassed = true;
+    record Test(float n, int t, long want) {}
+    Test[] tests = {
+      new Test(0.9f, 50, -1),
+      new Test(5.1f, 50, -1),
+      new Test(3.0f, -1, -1),
+      new Test(3.0f, 101, -1),
+      new Test(Float.NaN, 50, -1),
+      new Test(1.0f, 0, 0),
+      new Test(1.0f, 1, 0),
+      new Test(1.1f, 0, 0),
+      new Test(1.1f, 1, 0),
+      new Test(3.0f, 100, 0),
+      new Test(3.1f, 100, 1),
+      new Test(5.0f, 100, 1),
+    };
+    for (Test tt : tests) {
+      long got = callback.apply(tt.n, tt.t);
+      if (got != tt.want) {
+        System.out.printf("%s(%.1f, %d) = %d, want %d%n", name, tt.n, tt.t, got, tt.want);
+        isPassed = false;
+      }
+    }
+    if (isPassed) {
+      System.out.printf("%s tests passed%n", name);
+    }
+  }
+
   public static void main(String[] args) {
-    float n; // 内申点
-    int t; // 試験の点数
-    n = 4.5f;
-    t = 90;
-    System.out.println("内申:" + n + "  試験:" + t + "  結果:" + Judgement.judgement(n, t));
-    n = 2.0f;
-    t = 55;
-    System.out.println("内申:" + n + "  試験:" + t + "  結果:" + Judgement.judgement(n, t));
-    n = 0.5f;
-    t = 101;
-    System.out.println("内申:" + n + "  試験:" + t + "  結果:" + Judgement.judgement(n, t));
+    test(Judgement::judgement, "Judgement");
   }
 }
