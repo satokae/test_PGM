a8cf37efc25944c57fedde196bc8551ad8bfc670
satokae <208755468+satoKae@users.noreply.github.com>
2026-07-07 09:46:30 +0900

    format

--- test_PGM2/Main.java
+++ test_PGM2/Main.java
@@ -1,12 +1,16 @@
 public class Main {
-	public static void main(String[] args){
-		float n;	// 内申点 
-		int t;		// 試験の点数
-		n=4.5f; t=90;
-		System.out.println( "内申:" + n +"  試験:"+t+"  結果:"+ Judgement.judgement( n, t ));
-		n=2.0f; t=55;
-		System.out.println( "内申:" + n +"  試験:"+t+"  結果:"+ Judgement.judgement( n, t ));
-		n=0.5f; t=101;
-		System.out.println( "内申:" + n +"  試験:"+t+"  結果:"+ Judgement.judgement( n, t ));
-	}
+
+  public static void main(String[] args) {
+    float n; // 内申点
+    int t; // 試験の点数
+    n = 4.5f;
+    t = 90;
+    System.out.println("内申:" + n + "  試験:" + t + "  結果:" + Judgement.judgement(n, t));
+    n = 2.0f;
+    t = 55;
+    System.out.println("内申:" + n + "  試験:" + t + "  結果:" + Judgement.judgement(n, t));
+    n = 0.5f;
+    t = 101;
+    System.out.println("内申:" + n + "  試験:" + t + "  結果:" + Judgement.judgement(n, t));
+  }
 }
